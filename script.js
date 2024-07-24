b262c4187da731dba1466b1de9d15baef489af1b
nomirgithub <lailanomir3@gmail.com>
2024-07-25 01:10:01 +0300

    plant

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function Preloader() {
     setTimeout(() => {
         Preloader.style.display = 'none';
         mainDiv.style.display = 'block';
-    }, 3000);
+    }, 1000);
 }
 
 window.addEventListener('load', Preloader);

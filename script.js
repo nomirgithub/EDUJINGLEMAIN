29b1136f1e7f5a907cdefd5c6c637e2abb9cac93
nomirgithub <lailanomir3@gmail.com>
2024-07-29 20:35:50 +0300

    stories 22

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const articleBody = document.body;
+function darkMode() {
+    articleBody.style.backgroundColor = "white";
+    articleBody.style.color = "black";
+
+}
+function lightMode() {
+    articleBody.style.backgroundColor = "black";
+    articleBody.style.color = "white";
+
+}
+
+
+
 
 
 
